42a117c7f15161463e79812e6a4d09075c1f5029
Kason Suchow <kason.suchow@gmail.com>
2024-07-19 13:24:01 -0600

    Created the create file, but the dev key is required when launching... Not sure about using this package anymore.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import PySimpleGUI as sg
 
-
-###### EDIT THESE STRINGS ######
-
 header_text = "Your System Uptime Has Exceeded 24 Hours"
 body_text = """This is just a warning that your system uptime has exceeded 24 hours.\n
 If your system uptime exceeds 7 days you will be required to reboot.\n
@@ -10,8 +7,6 @@ We recommend saving and closing all programs and running any available\n
 windows updates followed by a reboot at your earliest convienience."""
 
 confirm_text = "Acknowledge"
-
-###### DO NOT TOUCH ######
 
 color_nexus_blue = "#0099D8"
 color_nexus_light_gray = "#EDEDEE"
@@ -27,18 +22,6 @@ header_object = sg.Text(
     size = (1000, 1),
     border_width=30
 )
-
-#body_object = sg.Multiline(
-#    default_text = body_text,
-#    background_color = color_nexus_light_gray,
-#    expand_x=True,
-#    expand_y=True,
-#    auto_size_text=True,
-#    font = ("Roboto", 14),
-#    justification="center",
-#    no_scrollbar=True,
-#    disabled=True,
-#)
  
 body_object = sg.Text(
     body_text,
