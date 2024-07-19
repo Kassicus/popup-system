740f8f6646d5009323a01b03ae15983a39bd9cef
Kason Suchow <kason.suchow@gmail.com>
2024-07-19 13:14:01 -0600

    This is pretty close to the other one...

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import PySimpleGUI as sg
 ###### EDIT THESE STRINGS ######
 
 header_text = "Your System Uptime Has Exceeded 24 Hours"
-body_text = "This is just a warning that your system uptime has exceeded 24 hours. If your system uptime exceeds 7 days you will be required to reboot. We recommend saving and closing all programs and running any available windows updates followed by a reboot at your earliest convienience."
+body_text = """This is just a warning that your system uptime has exceeded 24 hours.\n
+If your system uptime exceeds 7 days you will be required to reboot.\n
+We recommend saving and closing all programs and running any available\n
+windows updates followed by a reboot at your earliest convienience."""
+
 confirm_text = "Acknowledge"
 
 ###### DO NOT TOUCH ######
@@ -24,21 +28,30 @@ header_object = sg.Text(
     border_width=30
 )
 
-body_object = sg.Multiline(
-    default_text = body_text,
-    background_color = color_nexus_light_gray,
-    expand_x=True,
-    expand_y=True,
-    auto_size_text=True,
-    font = ("Roboto", 14),
-    justification="center",
-    no_scrollbar=True,
-    disabled=True,
+#body_object = sg.Multiline(
+#    default_text = body_text,
+#    background_color = color_nexus_light_gray,
+#    expand_x=True,
+#    expand_y=True,
+#    auto_size_text=True,
+#    font = ("Roboto", 14),
+#    justification="center",
+#    no_scrollbar=True,
+#    disabled=True,
+#)
+ 
+body_object = sg.Text(
+    body_text,
+    font = ("Roboto", 16),
+    text_color="Black",
+    background_color=color_nexus_light_gray,
+    expand_x = True,
+    justification="center"
 ) 
 
 ack_button = sg.Button(
     confirm_text,
-    font=("Roboto", 16),
+    font=("Roboto", 18),
     auto_size_button=True,
     button_color="Red",
     size=(40,1),
@@ -48,7 +61,7 @@ ack_button = sg.Button(
 layout = [
     [header_object],
     [sg.VPush(background_color=color_nexus_light_gray)],
-    [body_object],
+    [sg.Push(background_color=color_nexus_light_gray), body_object, sg.Push(background_color=color_nexus_light_gray)],
     [sg.VPush(background_color=color_nexus_light_gray)],
     [sg.Push(background_color=color_nexus_light_gray), ack_button, sg.Push(background_color=color_nexus_light_gray)]
 ]
@@ -58,12 +71,13 @@ window = sg.Window(
     layout,
     background_color = color_nexus_light_gray,
     disable_close = True,
+    disable_minimize=True,
     keep_on_top = True,
-    size = (800, 600),
+    size = (800, 500),
     grab_anywhere = True,
+    no_titlebar= True,
     margins = (0, 0),
     finalize = True,
-    #icon = "assets/logo.png" TODO: Fix this
 )
 
 window.ding()
